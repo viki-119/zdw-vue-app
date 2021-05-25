c0bf0dd7d512538209efec0132526ab82d528e47
viki-119 <1192756137@qq.com>
2021-05-25 21:57:48 +0800

    feat: 日常提交

--- src/config/router.js
+++ src/config/router.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import Goods from '../goods';
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
 // 我们后面再讨论嵌套路由。
 const routes = [
-  { path: '/', component: <div>123</div> },
+  { path: '/', component: Goods },
   { path: '/about', component: <div>456</div> },
 ];
 
---
+++ src/goods/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hehe</div>
+</template>
+
+<script>
+export default {
+}
+// import { defineComponent } from '@vue/composition-api'
+
+// export default defineComponent({
+//   setup() {
+    
+//   },
+// })
+</script>
